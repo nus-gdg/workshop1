c74e874c41cd2eca249865b493bcb749097bea4c
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-01-25 20:16:32 +0800

    Move Game to Common namespace

---
+++ Assets/Scripts/Common/Game.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Common
+{
+    public class Game : MonoBehaviour
+    {
+        private static Game _instance;
+        public static Game Instance => _instance;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Load()
+        {
+            _instance = Instantiate(Resources.Load<Game>("Game"));
+            DontDestroyOnLoad(_instance);
+        }
+    }
+}
