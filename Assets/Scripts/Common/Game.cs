3822e2ca5c88adaaac83eee7d5920c10be017366
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-01-26 21:40:55 +0800

    Add audio manager
    Notes:
    - AudioManager controls the volumes of musics and sounds.
    - To control different sound types:
        - Add a mixer group to the relevant Mixer prefabs
        - Inspect an audio source
        - Assign the mixer group for the audio source

--- Assets/Scripts/Common/Game.cs
+++ Assets/Scripts/Common/Game.cs
@@ -6,6 +6,10 @@ namespace Common
     {
         private static Game _instance;
         public static Game Instance => _instance;
+        
+        [SerializeField]
+        private new AudioManager audio;
+        public AudioManager Audio => audio;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Load()
