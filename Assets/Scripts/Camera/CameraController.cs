733b404832e7a6f878af4d54e531f18c4314dda7
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2021-01-31 02:32:54 +0800

    Add basic camera logic

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float SmoothTime = 0.3f;
+
+    public Vector3 TargetPosition
+    {
+        get { return targetPosition; }
+        set { targetPosition = value; }
+    }
+    private Vector3 targetPosition;
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        targetPosition.z = transform.position.z;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+    }
+}
---
+++ Assets/Scripts/Camera/CameraLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Camera))]
+[RequireComponent(typeof(CameraController))]
+public class CameraLogic : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public Transform Cursor;
+    private CameraController cameraController;
+    private Camera attachedCamera;
+
+    void Awake()
+    {
+        attachedCamera = GetComponent<Camera>();
+        Assert.IsNotNull(attachedCamera, "CameraController expects an attached camera");
+        cameraController = GetComponent<CameraController>();
+        Assert.IsNotNull(cameraController, "CameraController expects a CameraController");
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = PlayerTransform.position * 0.75f + Cursor.position * 0.25f;
+        cameraController.TargetPosition = targetPosition;
+    }
+}
