35cf875dcac873d4dddfab59c9824b27c095c867
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-01-30 23:25:47 +0800

    Add input manager to global managers
    Notes:
    - Modify input bindings in Resources/InputManager.inputactions

--- Assets/Scripts/Common/Game.cs
+++ Assets/Scripts/Common/Game.cs
@@ -6,11 +6,15 @@ namespace Common
     {
         private static Game _instance;
         public static Game Instance => _instance;
+        
+        private InputManager _input;
+        public InputManager Input => _input;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Load()
         {
             _instance = Instantiate(Resources.Load<Game>("Game"));
+            _instance._input = new InputManager();
             DontDestroyOnLoad(_instance);
         }
     }
