01117c50a7ed2a8c333d9ec81f203d91720145c5
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2021-01-31 15:01:49 +0800

    Add scene loading helper and pause function

--- Assets/Scripts/Common/CameraManager.cs
+++ Assets/Scripts/Common/CameraManager.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
 
 namespace Common
 {
     public class CameraManager : MonoBehaviour
     {
+        public void OnSceneLoadedByWorld(Scene scene)
+        {
+            // disable all new camera at root
+            // we can do more here
+            // if this is too slow we can do setup per scene for optimization but this is more convenient for everyone at the moment
+            foreach (GameObject go in scene.GetRootGameObjects())
+            {
+                if (go.GetComponent<Camera>())
+                {
+                    go.SetActive(false);
+                }
+            }
+        }
+
         public static Vector2 ClampWorldPositionInsideCamera2D(Vector2 position, Camera camera)
         {
             Vector2 camPos = camera.transform.position;
--- Assets/Scripts/Common/WorldManager.cs
+++ Assets/Scripts/Common/WorldManager.cs
@@ -1,4 +1,7 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 namespace Common
 {
@@ -12,11 +15,83 @@ namespace Common
         public CameraManager CameraManager => cameraManager;
 
         // TODO: Add system to pause entities
-
         void OnApplicationQuit()
         {
             Player = null;
             Cursor = null;
         }
+
+        public void PauseWorld()
+        {
+            Time.timeScale = 0.0f;
+        }
+
+        public void UnpauseWorld()
+        {
+            Time.timeScale = 1.0f;
+        }
+
+        public delegate void OnSceneLoadedFunc(Scene scene);
+        public delegate void OnSceneLoadFailFunc();
+
+        public void LoadScene(string name)
+        {
+            LoadScene(name, (scene) => { }, () => { });
+        }
+
+        public void LoadScene(string name, OnSceneLoadedFunc successFunc, OnSceneLoadFailFunc failFunc)
+        {
+            Scene scene = SceneManager.GetSceneByName(name);
+            if (scene.isLoaded)
+            {
+                // if scene is already loaded, just return success
+                // we don't want duplicate scenes
+                successFunc(scene);
+                return;
+            }
+            StartCoroutine(LoadSceneAsyncOperation(name, successFunc, failFunc));
+        }
+
+        public delegate void OnSceneUnloaded();
+
+        public void UnloadScene(string name)
+        {
+            StartCoroutine(UnloadSceneAsyncOperation(name, () => { }));
+        }
+        public void UnloadScene(string name, OnSceneUnloaded func)
+        {
+            StartCoroutine(UnloadSceneAsyncOperation(name, func));
+        }
+
+        IEnumerator LoadSceneAsyncOperation(string name, OnSceneLoadedFunc successFunc, OnSceneLoadFailFunc failFunc)
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+
+            Scene scene = SceneManager.GetSceneByName(name);
+            if (!scene.IsValid() || !scene.isLoaded)
+            {
+                failFunc();
+                yield break;
+            }
+
+            CameraManager.OnSceneLoadedByWorld(scene);
+            successFunc(scene);
+        }
+
+        IEnumerator UnloadSceneAsyncOperation(string name, OnSceneUnloaded func)
+        {
+            AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(name);
+            while (!asyncUnload.isDone)
+            {
+                yield return null;
+            }
+            func();
+        }
+
     }
 }
