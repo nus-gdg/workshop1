e5f9173e2630e90fd05bb4c8fb0e77a1c8709b41
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-01 19:59:03 +0800

    Add template for poolable bullets
    Notes:
    - Create an Initialize function to set up the behaviour of the bullet when it is created from a pool

---
+++ Assets/Scripts/Entity/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Common;
+
+namespace Entity
+{
+    public class Bullet : Poolable
+    {
+        [SerializeField]
+        private new Rigidbody2D rigidbody;
+
+        public Vector2 velocity;
+
+        // private TestBulletPool _pool;
+
+        protected override void OnAwake()
+        {
+        }
+
+        protected override void OnRecycle()
+        {
+            transform.position = Vector3.zero;
+        }
+
+        private void Start()
+        {
+            // _pool = Game.Instance.Pool.TestBullets;
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            rigidbody.position += velocity * Time.fixedDeltaTime;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Recycle();
+        }
+    }
+}
