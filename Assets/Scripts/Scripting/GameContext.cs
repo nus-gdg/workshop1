ffe6bd5d024c57fd52e8b9a9293ec6fb855b3e2d
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2021-02-06 00:13:39 +0800

    Clean files and update trigger component code

--- Assets/Scripts/Scripting/GameContext.cs
+++ Assets/Scripts/Scripting/GameContext.cs
@@ -6,4 +6,5 @@ public class GameContext
 {
     // we can expand on this
     public GameObject ContextEntity;
+    public GameObject SourceEntity;
 }
--- Assets/Scripts/Trigger/TriggerComponent2D.cs
+++ Assets/Scripts/Trigger/TriggerComponent2D.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
 public class TriggerComponent2D : MonoBehaviour
 {
     public GameEvent OnCollisionEnterEvent;
     public GameEvent OnCollisionExitEvent;
-    public GameEvent OnTriggerEnter2DEvent;
-    public GameEvent OnTriggerExit2DEvent;
+    public GameEvent OnTriggerEnterEvent;
+    public GameEvent OnTriggerExitEvent;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         GameContext context = new GameContext();
         context.ContextEntity = collision.gameObject;
+        context.SourceEntity = gameObject;
         OnCollisionEnterEvent.Invoke(context);
     }
 
@@ -21,6 +23,7 @@ public class TriggerComponent2D : MonoBehaviour
     {
         GameContext context = new GameContext();
         context.ContextEntity = collision.gameObject;
+        context.SourceEntity = gameObject;
         OnCollisionExitEvent.Invoke(context);
     }
 
@@ -28,13 +31,15 @@ public class TriggerComponent2D : MonoBehaviour
     {
         GameContext context = new GameContext();
         context.ContextEntity = other.gameObject;
-        OnTriggerEnter2DEvent.Invoke(context);
+        context.SourceEntity = gameObject;
+        OnTriggerEnterEvent.Invoke(context);
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         GameContext context = new GameContext();
         context.ContextEntity = other.gameObject;
-        OnTriggerExit2DEvent.Invoke(context);
+        context.SourceEntity = gameObject;
+        OnTriggerExitEvent.Invoke(context);
     }
 }
