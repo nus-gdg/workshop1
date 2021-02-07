7820685e6fd92b400c53b0ff82b6b4272df59c19
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2021-02-07 12:58:48 +0800

    Add stress test case for Daniel

--- Assets/!Test/World/Combat/TestingScript.cs
+++ Assets/!Test/World/Combat/TestingScript.cs
@@ -115,7 +115,60 @@ namespace Testing
                 Assert.AreEqual(damageParams.DamageAmount, 15);
                 handler.CombatStats = null;
             }
+
+            {
+                Element element = ScriptableObject.CreateInstance<Element>();
+
+                CombatStats sourceCombatStats = ScriptableObject.CreateInstance<CombatStats>();
+                sourceCombatStats.DamageModifiers = new List<DamageModifier>();
+                CombatStats targetCombatStats = ScriptableObject.CreateInstance<CombatStats>();
+                targetCombatStats.DamageModifiers = new List<DamageModifier>();
+                handler.CombatStats = targetCombatStats;
+
+                FunctionOperation operation = new FunctionOperation();
+                operation.Operation = FunctionOperation.OperationType.Add;
+                operation.Value = 3f;
+
+                FunctionOperation operation2 = new FunctionOperation();
+                operation2.Operation = FunctionOperation.OperationType.Multiply;
+                operation2.Value = 3f;
+
+                ElementDamageModifier modifier = ScriptableObject.CreateInstance<ElementDamageModifier>();
+                modifier.Element = element;
+                modifier.Function.Operations = new FunctionOperation[2] { operation, operation2 };
+
+                for (int i = 0; i < 1000; ++i)
+                    targetCombatStats.DamageModifiers.Add(modifier);
+
+
+                var watch = new System.Diagnostics.Stopwatch();
+
+                watch.Start();
+                for (int i = 0; i < 100000; i++)
+                {
+                    operation.Value = i;
+                    operation2.Value = i;
+
+                    DamageParams damageParams = new DamageParams();
+                    damageParams.DamageAmount = 15;
+                    damageParams.SourceStats = sourceCombatStats;
+                    damageParams.SourceEntity = gameObject;
+                    damageParams.Element = element; // not the same element
+
+                    handler.ApplyDamage(ref damageParams);
+                }
+                watch.Stop();
+                Debug.Log("Test Case Runtime: " + watch.ElapsedMilliseconds + "ms");
+            }
+
         }
 
+        void Update()
+        {
+            for (int i = 0; i < 1; ++i)
+            {
+                DamageParams p = new DamageParams();
+            }
+        }
     }
 }
