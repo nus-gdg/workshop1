0b64a878c3c82b4c274af9171316cb265492dfad
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-14 00:08:14 +0800

    Add extendable enums for floors

---
+++ Assets/!Sandbox/Gabriel's Movement/FloorType.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "New Floor", menuName = "Floor Type")]
+    public class FloorType : ScriptableObject
+    {
+    }
+}
