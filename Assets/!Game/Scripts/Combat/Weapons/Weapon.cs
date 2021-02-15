bd545652f1349dda28720c28b322ed8c34d3e75c
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-15 17:28:08 +0800

    Move Weapon to Combat.Weapons namespace

---
+++ Assets/!Game/Scripts/Combat/Weapons/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Combat.Weapons
+{
+    public class Weapon : MonoBehaviour
+    {
+        public Transform firePoint;
+    
+        public Plant plant;
+
+        public float bulletForce = 20f;
+
+        public float shotInterval = 1;
+        public bool canShoot = true;
+
+        public void Attack ()
+        {
+            if (canShoot)
+            {
+                StartCoroutine(ShootDelay());
+
+                GameObject bullet = Instantiate(plant.bullet, firePoint.position, firePoint.rotation);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(transform.right * bulletForce, ForceMode2D.Impulse);
+            }
+        }
+
+        IEnumerator ShootDelay()
+        {
+            canShoot = false;
+            yield return new WaitForSeconds(shotInterval);
+            canShoot = true;
+        }
+    }
+}
