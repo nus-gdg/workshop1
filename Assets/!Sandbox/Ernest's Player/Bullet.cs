0d449b043a76e66934eb92244a7f62baaeff3719
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-15 15:21:27 +0800

    Add properties in Bullet for required components

--- Assets/!Sandbox/Ernest's Player/Bullet.cs
+++ Assets/!Sandbox/Ernest's Player/Bullet.cs
@@ -1,14 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Combat;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    
-    void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField]
+    private new Collider2D collider;
+    [SerializeField]
+    private new Rigidbody2D rigidbody;
+    [SerializeField]
+    private HitBox hitBox;
+
+    public Collider2D Collider => collider;
+    public Rigidbody2D Rigidbody2D => rigidbody;
+    public HitBox HitBox => hitBox;
+
+    private void Start()
     {
-        
-        Destroy(gameObject);
+        if (collider == null)
+        {
+            collider = GetComponent<Collider2D>();
+            if (collider == null)
+            {
+                throw new MissingComponentException("Missing component: Collider2D");
+            }
+        }
+        if (rigidbody == null)
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            if (rigidbody == null)
+            {
+                throw new MissingComponentException("Missing component: Rigidbody2D");
+            }
+        }
+        if (hitBox == null)
+        {
+            hitBox = GetComponent<HitBox>();
+            if (hitBox == null)
+            {
+                throw new MissingComponentException("Missing component: HitBox");
+            }
+        }
     }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
 }
