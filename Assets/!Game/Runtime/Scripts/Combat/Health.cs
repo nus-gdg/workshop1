8f86ff31753a5eacfb646458272b6549fb01d10b
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-15 16:04:08 +0800

    Move Health script to Combat namespace

---
+++ Assets/!Game/Runtime/Scripts/Combat/Health.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Combat
+{
+    public class Health : MonoBehaviour
+    {
+        // To do (from Gabriel and Daniel): 
+        // Make certain fields readonly? Add event system for death. 
+
+        public int maxHealth = 100;
+        public int currentHealth;
+        public int dieValue = 0;
+        public Slider healthSlider;
+        public int fortesting = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            /*
+            ResetHealth();
+            */
+        }
+
+        // Update is called once per frame
+        /*
+        void Update()
+        {
+            if (fortesting > 1) {
+                Damage(30);
+                fortesting = 0;
+            }
+        }
+        */
+
+        //do damage
+        public void Damage(int amt) {
+            currentHealth -= amt;
+            if (currentHealth < dieValue) {
+                Die();
+            }
+
+            healthSlider.value = (float) currentHealth/maxHealth;
+        }
+
+        void Die() {
+            //what to do when dead
+            Debug.Log("deadlol");
+            // ResetHealth();
+        }
+
+        void ResetHealth() {
+            currentHealth = maxHealth;
+            healthSlider.value = 1;
+        }
+    }
+}
