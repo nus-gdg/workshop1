dab8939f68f8dfc0f8549cf0f116f0170dad6c3f
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2021-02-17 00:29:48 +0800

    Add negate condition

---
+++ Assets/!Game/Runtime/Scripts/Core/Events/GameConditions/NegateCondition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Events;
+
+namespace Experimental
+{
+
+    public class NegateCondition : GameCondition
+    {
+        [SerializeReference]
+        [SubclassSelectorAttribute(typeof(GameCondition))]
+        GameCondition Condition;
+
+        public override bool Evaluate(GameContext context)
+        {
+            return Condition != null ? !Condition.Evaluate(context) : false;
+        }
+    }
+
+}
