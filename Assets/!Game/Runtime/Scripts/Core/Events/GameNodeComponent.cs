1554008f9658fb06bd4010c290b9ba61fd160974
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2021-02-16 23:58:11 +0800

    Use a separate component for node scripts - hookable with GameEvent

---
+++ Assets/!Game/Runtime/Scripts/Core/Events/GameNodeComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Events;
+
+namespace Experimental
+{
+    public class GameNodeComponent : MonoBehaviour
+    {
+        [SerializeReference]
+        [SubclassSelectorAttribute(typeof(GameNode))]
+        public GameNode GameNode;
+        public void Evaluate(GameContext context)
+        {
+            GameNode.Evaluate(context);
+        }
+    }
+}
+
