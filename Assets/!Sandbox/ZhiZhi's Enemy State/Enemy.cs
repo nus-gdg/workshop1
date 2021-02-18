77a87e379ee462084cb14b73d29f260f8364fbec
zhizhizhizhizhi <zhizhi.xu@yahoo.com>
2021-02-18 20:25:29 +0800

    nothing much

---
+++ Assets/!Sandbox/ZhiZhi's Enemy State/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed;
+    public float timeBetweenMove;
+    public float timeToMove;
+    public State state;
+
+    private Rigidbody2D rigidbody;
+    private bool moving;
+    private Vector3 moveDirection;
+    private float timeBetweenMoveCounter;
+    private float timeToMoveCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        timeBetweenMove = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+        timeToMove = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f); 
+        moving = (Random.Range(-1,1) > 0);
+        timeBetweenMoveCounter = timeBetweenMove;
+        timeToMoveCounter = timeToMove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving) {
+            timeToMoveCounter -= Time.deltaTime;
+            rigidbody.velocity = moveDirection;
+
+            if (timeToMoveCounter < 0f) {
+                moving = false;
+                timeBetweenMoveCounter = timeBetweenMove;
+            }
+
+        } else {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            rigidbody.velocity = Vector2.zero;
+
+            if (timeBetweenMoveCounter < 0f) {
+                moving = true;
+                timeToMoveCounter = timeToMove;
+
+                moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0);
+            }
+        }
+    }
+}
---
+++ Assets/!Sandbox/ZhiZhi's Enemy State/State.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(filename="State", menuname="State")]
+public class State : ScriptableObject
+{
+    class StateAction {
+
+    }
+    
+    class StateDecision {
+
+    }
+    
+    void Act()
+    {
+        
+    }
+
+    void Decide()
+    {
+        
+    }
+}
