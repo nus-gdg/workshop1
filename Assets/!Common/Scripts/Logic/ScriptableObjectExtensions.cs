07a17ec19f0cdd1041d9d999f9d0bca2d2cb15b9
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-23 23:25:01 +0800

    Refactor tabs in ScriptableObjectExtensions

--- Assets/!Common/Scripts/Logic/ScriptableObjectExtensions.cs
+++ Assets/!Common/Scripts/Logic/ScriptableObjectExtensions.cs
@@ -8,112 +8,112 @@ namespace Common.Logic
 {
     public static class ScriptableObjectExtensions
     {
-		#if UNITY_EDITOR
+#if UNITY_EDITOR
 
-	    public static bool HasMissingScripts<T>(this T instance) where T : ScriptableObject
-	    {
-		    //Check for missing scripts
-		    var path = AssetDatabase.GetAssetPath(instance);
-		    var scripts = AssetDatabase.LoadAllAssetRepresentationsAtPath(path);
+        public static bool HasMissingScripts<T>(this T instance) where T : ScriptableObject
+        {
+            //Check for missing scripts
+            var path = AssetDatabase.GetAssetPath(instance);
+            var scripts = AssetDatabase.LoadAllAssetRepresentationsAtPath(path);
 
-		    foreach (var script in scripts)
-		    {
-			    if (script == null)
-			    {
-				    return true;
-			    }
-		    }
-		    return false;
-	    }
+            foreach (var script in scripts)
+            {
+                if (script == null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-	    /// <summary>
-		/// Function used to remove a sub-asset that is missing the script reference
-		/// </summary>
-		/// <param name="instance">The main asset that holds the sub-asset</param>
-		public static void FixMissingScripts<T>(this T instance) where T : ScriptableObject
-		{
-			if (!HasMissingScripts(instance))
-			{
-				return;
-			}
+        /// <summary>
+        /// Function used to remove a sub-asset that is missing the script reference
+        /// </summary>
+        /// <param name="instance">The main asset that holds the sub-asset</param>
+        public static void FixMissingScripts<T>(this T instance) where T : ScriptableObject
+        {
+            if (!HasMissingScripts(instance))
+            {
+                return;
+            }
 
-			//Create a new instance of the object to delete
-			ScriptableObject newInstance = ScriptableObject.CreateInstance<T>();
+            //Create a new instance of the object to delete
+            ScriptableObject newInstance = ScriptableObject.CreateInstance<T>();
 
-			//Copy the original content to the new instance
-			EditorUtility.CopySerialized(instance, newInstance);
-			newInstance.name = instance.name;
+            //Copy the original content to the new instance
+            EditorUtility.CopySerialized(instance, newInstance);
+            newInstance.name = instance.name;
 
-			string instancePath = AssetDatabase.GetAssetPath(instance);
-			string clonePath = instancePath.Replace(".asset", "CLONE.asset");
+            string instancePath = AssetDatabase.GetAssetPath(instance);
+            string clonePath = instancePath.Replace(".asset", "CLONE.asset");
 
-			//Create the new asset on the project files
-			AssetDatabase.CreateAsset(newInstance, clonePath);
-			AssetDatabase.ImportAsset(clonePath);
+            //Create the new asset on the project files
+            AssetDatabase.CreateAsset(newInstance, clonePath);
+            AssetDatabase.ImportAsset(clonePath);
 
-			//Unhide sub-assets
-			var subAssets = AssetDatabase.LoadAllAssetsAtPath(instancePath);
-			HideFlags[] flags = new HideFlags[subAssets.Length];
-			for (int i = 0; i < subAssets.Length; i++)
-			{
-				//Ignore the "corrupt" one
-				if (subAssets[i] == null)
-					continue;
+            //Unhide sub-assets
+            var subAssets = AssetDatabase.LoadAllAssetsAtPath(instancePath);
+            HideFlags[] flags = new HideFlags[subAssets.Length];
+            for (int i = 0; i < subAssets.Length; i++)
+            {
+                //Ignore the "corrupt" one
+                if (subAssets[i] == null)
+                    continue;
 
-				//Store the previous hide flag
-				flags[i] = subAssets[i].hideFlags;
-				subAssets[i].hideFlags = HideFlags.None;
-				EditorUtility.SetDirty(subAssets[i]);
-			}
+                //Store the previous hide flag
+                flags[i] = subAssets[i].hideFlags;
+                subAssets[i].hideFlags = HideFlags.None;
+                EditorUtility.SetDirty(subAssets[i]);
+            }
 
-			EditorUtility.SetDirty(instance);
-			AssetDatabase.SaveAssets();
+            EditorUtility.SetDirty(instance);
+            AssetDatabase.SaveAssets();
 
-			//Reparent the subAssets to the new instance
-			foreach (var subAsset in AssetDatabase.LoadAllAssetRepresentationsAtPath(instancePath))
-			{
-				//Ignore the "corrupt" one
-				if (subAsset == null)
-					continue;
+            //Reparent the subAssets to the new instance
+            foreach (var subAsset in AssetDatabase.LoadAllAssetRepresentationsAtPath(instancePath))
+            {
+                //Ignore the "corrupt" one
+                if (subAsset == null)
+                    continue;
 
-				//We need to remove the parent before setting a new one
-				AssetDatabase.RemoveObjectFromAsset(subAsset);
-				AssetDatabase.AddObjectToAsset(subAsset, newInstance);
-			}
+                //We need to remove the parent before setting a new one
+                AssetDatabase.RemoveObjectFromAsset(subAsset);
+                AssetDatabase.AddObjectToAsset(subAsset, newInstance);
+            }
 
-			//Import both assets back to unity
-			AssetDatabase.ImportAsset(instancePath);
-			AssetDatabase.ImportAsset(clonePath);
+            //Import both assets back to unity
+            AssetDatabase.ImportAsset(instancePath);
+            AssetDatabase.ImportAsset(clonePath);
 
-			//Reset sub-asset flags
-			for (int i = 0; i < subAssets.Length; i++)
-			{
-				//Ignore the "corrupt" one
-				if (subAssets[i] == null)
-					continue;
+            //Reset sub-asset flags
+            for (int i = 0; i < subAssets.Length; i++)
+            {
+                //Ignore the "corrupt" one
+                if (subAssets[i] == null)
+                    continue;
 
-				subAssets[i].hideFlags = flags[i];
-				EditorUtility.SetDirty(subAssets[i]);
-			}
+                subAssets[i].hideFlags = flags[i];
+                EditorUtility.SetDirty(subAssets[i]);
+            }
 
-			EditorUtility.SetDirty(newInstance);
-			AssetDatabase.SaveAssets();
+            EditorUtility.SetDirty(newInstance);
+            AssetDatabase.SaveAssets();
 
-			//Here's the magic. First, we need the system path of the assets
-			string globalToDeletePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), instancePath);
-			string globalClonePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), clonePath);
+            //Here's the magic. First, we need the system path of the assets
+            string globalToDeletePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), instancePath);
+            string globalClonePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), clonePath);
 
-			//We need to delete the original file (the one with the missing script asset)
-			//Rename the clone to the original file and finally
-			//Delete the meta file from the clone since it no longer exists
+            //We need to delete the original file (the one with the missing script asset)
+            //Rename the clone to the original file and finally
+            //Delete the meta file from the clone since it no longer exists
 
-			File.Delete(globalToDeletePath);
-			File.Delete(globalClonePath + ".meta");
-			File.Move(globalClonePath, globalToDeletePath);
+            File.Delete(globalToDeletePath);
+            File.Delete(globalClonePath + ".meta");
+            File.Move(globalClonePath, globalToDeletePath);
 
-			AssetDatabase.Refresh();
-		}
+            AssetDatabase.Refresh();
+        }
 
-		#endif
+#endif
     }
 }
