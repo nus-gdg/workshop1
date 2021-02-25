0bc452021c325d5f9fd4c53d9a557e28ac363413
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-25 23:17:39 +0800

    Add blackboard variables
    Notes:
    - Blackboard variables are scriptable objects that store values.
    - These can be attached to any component to easily access important values.
    - Eg. You can write the cursor position into a variable. Then other object can read the position via drag and drop.

---
+++ Assets/!Common/Scripts/Logic/Blackboard/Variables/TransformVariable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Common.Logic
+{
+    [CreateAssetMenu(fileName = "Transform", menuName = "Variable/Transform", order = 0)]
+    public class TransformVariable : Variable<Transform>
+    {
+        
+    }
+}
