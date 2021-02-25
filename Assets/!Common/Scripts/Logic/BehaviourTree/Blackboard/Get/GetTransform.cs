dcb28b86107c74aea097b1a327a0cdf45ec8edf9
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-25 23:20:34 +0800

    Add getter nodes for behaviour tree blackboards

---
+++ Assets/!Common/Scripts/Logic/BehaviourTree/Blackboard/Get/GetTransform.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Common.Logic
+{
+    [CreateNodeMenu("Blackboard/Get/Transform", -100)]
+    public class GetTransform : GetNode<Transform, TransformVariable>
+    {
+        
+    }
+}
---
+++ Assets/!Common/Scripts/Logic/BehaviourTree/Blackboard/Get/GetVector2.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Common.Logic
+{
+    [CreateNodeMenu("Blackboard/Get/Vector2", -100)]
+    public class GetVector2 : GetNode<Vector2, Vector2Variable>
+    {
+        
+    }
+}
