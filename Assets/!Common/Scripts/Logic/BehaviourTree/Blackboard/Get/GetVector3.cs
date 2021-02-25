5ae58db2afeba7e25280e5ea1f3ba2d723eda76f
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-02-25 23:20:34 +0800

    Add getter nodes for behaviour tree blackboards

---
+++ Assets/!Common/Scripts/Logic/BehaviourTree/Blackboard/Get/GetVector3.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Common.Logic
+{
+    [CreateNodeMenu("Blackboard/Get/Vector3", -100)]
+    public class GetVector3 : GetNode<Vector3, Vector3Variable>
+    {
+        
+    }
+}
