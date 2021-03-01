15c2d48e9e073c3b00be8106504d4112dac05192
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2021-03-01 20:55:38 +0800

    Add parallel node (to be removed with Gab's changes)

---
+++ Assets/!Common/Scripts/Logic/BehaviourTree/Base/Composites/Parallel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+namespace Common.Logic
+{
+    [CreateNodeMenu("Behaviour Tree/Composite/Parallel Single Threaded", -100)]
+    public class Parallel : CompositeNode
+    {
+        public override Status Evaluate(BehaviourTreeController controller)
+        {
+            bool allCompleted = true;
+            for (int i = 0; i < children.Count; i++)
+            {
+                var child = children[i];
+                if (child == null)
+                {
+                    continue;
+                }
+
+                var result = child.Evaluate(controller);
+                controller.RegisterNodeStatus(child, result);
+                allCompleted &= result == Status.Completed;
+
+                if (result == Status.Failed)
+                {
+                    return Status.Failed;
+                }
+
+            }
+            return allCompleted ? Status.Completed : Status.Running;
+        }
+    }
+}
