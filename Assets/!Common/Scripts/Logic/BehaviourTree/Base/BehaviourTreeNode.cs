f886146cc9e02fd8f7a1054837be9c07e015ce07
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-03-01 11:45:26 +0800

    Rework behaviour tree nodes as event handlers
    Notes:
    - Tick replaces the exisiting Evaluate function in all nodes.
    - Tick encapsulates the Enter, Evaluate and Exit functions so that the status of controllers are correctly registered.

--- Assets/!Common/Scripts/Logic/BehaviourTree/Base/BehaviourTreeNode.cs
+++ Assets/!Common/Scripts/Logic/BehaviourTree/Base/BehaviourTreeNode.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using XNode;
 #if UNITY_EDITOR
 using System.Linq;
@@ -15,7 +17,79 @@ namespace Common.Logic
         }
 
         public BehaviourTree Graph => graph as BehaviourTree;
-        public abstract Status Evaluate(BehaviourTreeController controller);
+
+        private Dictionary<BehaviourTreeController, Status> _statusOfControllers =
+            new Dictionary<BehaviourTreeController, Status>();
+
+        public virtual void Load(BehaviourTreeController controller)
+        {
+            _statusOfControllers[controller] = Status.Ready;
+        }
+        
+        public virtual void Unload(BehaviourTreeController controller)
+        {
+            _statusOfControllers.Remove(controller);
+        }
+
+        public Status GetStatus(BehaviourTreeController controller)
+        {
+            if (!_statusOfControllers.TryGetValue(controller, out Status status))
+            {
+                return Status.Ready;
+            }
+            return status;
+        }
+        
+        public bool IsStatus(BehaviourTreeController controller, Status status)
+        {
+            if (!_statusOfControllers.TryGetValue(controller, out Status internalStatus))
+            {
+                return false;
+            }
+            return status == internalStatus;
+        }
+        
+        public void SetStatus(BehaviourTreeController controller, Status status)
+        {
+            _statusOfControllers[controller] = status;
+        }
+
+        public Status Tick(BehaviourTreeController controller)
+        {
+            var result = Status.Completed;
+
+            if (!IsStatus(controller, Status.Running))
+            {
+                Enter(controller);
+            }
+
+            result = Evaluate(controller);
+
+            if (result != Status.Running)
+            {
+                Exit(controller);
+            }
+
+            SetStatus(controller, result);
+            return result;
+        }
+
+        public virtual Status Evaluate(BehaviourTreeController controller)
+        {
+            throw new NotImplementedException($"{name} is missing an evaluate function.");
+        }
+
+        public virtual void Enter(BehaviourTreeController controller) { }
+        public virtual void Exit(BehaviourTreeController controller) { }
+
+        protected BehaviourTreeNode GetConnectedNode(NodePort port)
+        {
+            if (!port.IsConnected)
+            {
+                return null;
+            }
+            return port.GetConnection(0).node as BehaviourTreeNode;
+        }
 
         public override object GetValue(NodePort nodePort)
         {
