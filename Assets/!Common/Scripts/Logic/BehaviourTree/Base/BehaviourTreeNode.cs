f402761fcb0b6f4b986fd32f8da70e01e0fed6f8
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-03-01 11:32:01 +0800

    Add custom node editor for Behaviour Tree nodes
    Notes:
    - This separates the drawing of the properties and ports for Behaviour Tree nodes, making it easier to arrange the layout of the nodes.

--- Assets/!Common/Scripts/Logic/BehaviourTree/Base/BehaviourTreeNode.cs
+++ Assets/!Common/Scripts/Logic/BehaviourTree/Base/BehaviourTreeNode.cs
@@ -1,4 +1,9 @@
 using XNode;
+#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+using XNodeEditor;
+#endif
 
 namespace Common.Logic
 {
@@ -17,4 +22,38 @@ namespace Common.Logic
             return this;
         }
     }
+
+    #if UNITY_EDITOR
+    [CustomNodeEditor(typeof(BehaviourTreeNode))]
+    public class BehaviourTreeNodeEditor : NodeEditor
+    {
+        /// <summary> Draws standard field editors for all public fields </summary>
+        public override void OnBodyGUI()
+        {
+            serializedObject.Update();
+            DrawProperties();
+            serializedObject.ApplyModifiedProperties();
+        }
+        
+        /// <summary> Draws standard field editors for all public fields </summary>
+        protected void DrawProperties()
+        {
+            // Exclude properties with these names
+            string[] excludes = {"m_Script", "graph", "position", "ports", "parent", "child", "children"};
+
+            // Iterate through serialized properties and draw them like the Inspector (But with ports)
+            SerializedProperty iterator = serializedObject.GetIterator();
+            bool enterChildren = true;
+            while (iterator.NextVisible(enterChildren))
+            {
+                enterChildren = false;
+                if (excludes.Contains(iterator.name))
+                {
+                    continue;
+                }
+                NodeEditorGUILayout.PropertyField(iterator, true);
+            }
+        }
+    }
+    #endif
 }
