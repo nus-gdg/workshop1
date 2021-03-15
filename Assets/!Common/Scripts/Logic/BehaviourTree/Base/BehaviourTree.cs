5e11701b322cf9c561c410f6e83c6e1386165c4d
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-03-16 05:04:26 +0800

    Add behaviour tree docs

--- Assets/!Common/Scripts/Logic/BehaviourTree/Base/BehaviourTree.cs
+++ Assets/!Common/Scripts/Logic/BehaviourTree/Base/BehaviourTree.cs
@@ -14,11 +14,20 @@ namespace Common.Logic
     [RequireNode(typeof(RootNode))]
     public class BehaviourTree : NodeGraph
     {
+        /// <summary>
+        /// The root node of the behaviour tree.
+        /// </summary>
         public RootNode root;
         
+        /// <summary>
+        /// The list of monitored nodes for each controller using this behaviour tree.
+        /// </summary>
         public Dictionary<BehaviourTreeController, HashSet<Monitor>> monitorsByController =
             new Dictionary<BehaviourTreeController, HashSet<Monitor>>();
-        
+
+#if UNITY_EDITOR
+
+        // Graph editor colour settings
         public Color nodeCompleted = new Color32(45, 76, 37, 255);
         public Color nodeRunning = new Color32(76, 66, 33, 255);
         public Color nodeFailed = new Color32(76, 39, 28, 255);
@@ -26,28 +35,42 @@ namespace Common.Logic
         public Color nodeReady = new Color32(76, 41, 72, 255);
         public Color nodeDefault = new Color32(90, 97, 105, 255);
 
+        /// <summary>
+        /// Highlight the status of this controller in the graph editor.
+        /// </summary>
         [NonSerialized]
         public BehaviourTreeController SelectedController;
+#endif
 
+        /// <summary>
+        /// Adds the controller to the behaviour tree.
+        /// </summary>
         public virtual void LoadController(BehaviourTreeController controller)
         {
             root.LoadController(controller);
             monitorsByController[controller] = new HashSet<Monitor>();
         }
-        
+
+        /// <summary>
+        /// Removes the controller from the behaviour tree.
+        /// </summary>
         public virtual void ClearController(BehaviourTreeController controller)
         {
             root.ClearController(controller);
             monitorsByController.Remove(controller);
         }
 
+        /// <summary>
+        /// Ticks the behaviour tree.
+        /// Monitored nodes are ticked before other nodes.
+        /// </summary>
         public BehaviourTreeNode.Status Tick(BehaviourTreeController controller)
         {
             TickMonitors(controller);
             return root.Tick(controller);
         }
 
-        public void TickMonitors(BehaviourTreeController controller)
+        private void TickMonitors(BehaviourTreeController controller)
         {
             foreach (var monitor in monitorsByController[controller])
             {
@@ -59,19 +82,30 @@ namespace Common.Logic
             }
         }
 
+        /// <summary>
+        /// Adds a monitor node to the behaviour tree asset.
+        /// </summary>
         public void AddMonitor(BehaviourTreeController controller, Monitor monitor)
         {
             monitorsByController[controller].Add(monitor);
         }
         
+        /// <summary>
+        /// Removes a monitor node from the behaviour tree asset.
+        /// </summary>
         public void RemoveMonitor(BehaviourTreeController controller, Monitor monitor)
         {
             monitorsByController.Remove(controller);
         }
 
+        /// <summary>
+        /// Adds a node to the behaviour tree asset.
+        /// </summary>
         public override Node AddNode(Type type)
         {
             var node = base.AddNode(type);
+            
+            // Automatically create and reference the root node
             if (type == typeof(RootNode))
             {
                 root = node as RootNode;
