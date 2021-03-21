c817c2bc9554cbef0744d0289bda4c75ccdc8a3e
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2021-03-21 17:02:13 +0800

    Test timed bullet aoe

---
+++ Assets/!Game/Scripts/Entity/Tasks/Destroy.cs
@@ -0,0 +1,28 @@
+﻿using Common.Logic;
+using UnityEngine;
+
+namespace Entity.Tasks
+{
+    public class Destroy : TaskNode
+    {
+        [Header("Input")]
+        public BlackboardKey objectToDestroy;
+
+        public override Status Evaluate(BehaviourTreeController controller)
+        {
+            if (controller.TryGetValue(objectToDestroy, out Transform transformValue))
+            {
+                Destroy(transformValue.gameObject);
+                return Status.Completed;
+            }
+
+            if (controller.TryGetValue(objectToDestroy, out Object objectValue))
+            {
+                Destroy(objectValue);
+                return Status.Completed;
+            }
+            return Status.Failed;
+        }
+    }
+
+}
