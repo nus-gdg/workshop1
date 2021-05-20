b55bffc9ec1a4090aca24afb5f0ecb7151fb68e1
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-05-20 16:40:11 +0800

    feat: Restructure Game prefab
    Changes:
    - Renamed all project namespaces
    - Restructured game to have a Model-View-ViewModel (MVVM) architecture
    
    Notes:
    - Breaks existing functionality. Please do not rebase until here.

--- Assets/!Game/Scripts/Game.cs
+++ Assets/!Game/Scripts/Game.cs
@@ -1,45 +1,87 @@
-using Core.Managers;
+using Project.Common;
+using Project.Controllers;
+using Project.Data;
+using Project.Models;
+using Project.Views;
 using UnityEngine;
 
-namespace Core
+namespace Project
 {
     public class Game : MonoBehaviour
     {
-        private static Game _instance;
-        public static Game Instance => _instance;
+        public static Game Instance { get; private set; }
 
-        [SerializeField]
-        private WorldManager world;
-        public WorldManager World => world;
+        [SerializeField] private GameObject uiContainer;
+        [SerializeField] private GameObject logicContainer;
+        [SerializeField] private GameObject modelContainer;
+        [SerializeField] private GameObject storageContainer;
+        [SerializeField] private GameObject configContainer;
 
-        [SerializeField]
-        private new AudioManager audio;
-        public AudioManager Audio => audio;
+        [SerializeField] private string configFilePath = "config.json";
 
-        [SerializeField]
-        private StoryManager story;
-        public StoryManager Story => story;
-
-        [SerializeField]
-        private LevelManager levels;
-        public LevelManager Levels => levels;
-
-        [SerializeField]
-        private PoolManager pool;
-        public PoolManager Pool => pool;
-
-        private InputManager _input;
-        public InputManager Input => _input;
+        public IUi Ui { get; private set; }
+        public ILogic Logic { get; private set; }
+        public IModel Model { get; private set; }
+        public IStorage Storage { get; private set; }
+        public Config Config { get; private set; }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Load()
         {
-            _instance = Instantiate(Resources.Load<Game>("Game"));
+            Instance = Instantiate(Resources.Load<Game>("Game"));
+            DontDestroyOnLoad(Instance);
+        }
 
-            _instance._input = new InputManager();
-            _instance._input.Player.Enable();
+        private void Awake()
+        {
+            InitConfig();
+            InitStorage(Config);
+            InitModel(Storage);
+            InitLogic(Model, Storage);
+            InitUi(Logic);
+        }
 
-            DontDestroyOnLoad(_instance);
+        private void InitConfig()
+        {
+            Config = configContainer.GetComponent<Config>();
+            Config.Init(GetAssetFilePath(configFilePath));
+        }
+
+        private void InitStorage(Config config)
+        {
+            Storage = storageContainer.GetComponent<IStorage>();
+
+            string saveDataFilePath = GetAssetFilePath(config.SaveDataFilePath);
+            string userPrefsFilePath = GetAssetFilePath(config.UserPrefsFilePath);
+
+            Storage.Init(saveDataFilePath, userPrefsFilePath);
+        }
+
+        private void InitModel(IStorage storage)
+        {
+            Model = modelContainer.GetComponent<IModel>();
+
+            SaveData saveData = storage.ReadSaveData();
+            UserPrefs userPrefs = storage.ReadUserPrefs();
+
+            Model.Init(saveData, userPrefs);
+        }
+
+        private void InitLogic(IModel model, IStorage storage)
+        {
+            Logic = logicContainer.GetComponent<ILogic>();
+            Logic.Init(model, storage);
+        }
+
+        private void InitUi(ILogic logic)
+        {
+            Ui = uiContainer.GetComponent<IUi>();
+            Ui.Init(logic);
+        }
+
+        private string GetAssetFilePath(string filePath)
+        {
+            return $"{Application.dataPath}/{filePath}";
         }
     }
 }
