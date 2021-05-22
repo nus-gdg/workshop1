a6933cbd8e2dbe6f9b90d7b156c7bb624721b1bf
jialin7878 <cjl@u.nus.edu>
2021-05-22 18:07:18 +0800

    add walking animation/blend tree

---
+++ Assets/!Sandbox/Jialin's Art/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Project.Views.World.Entities {
+  public class PlayerAnimation : MonoBehaviour
+  {
+      //private InputManager.PlayerActions _controls;
+      private Animator anim;
+      public PlayerUi player;
+      // Start is called before the first frame update
+      void Start()
+      {
+          anim = GetComponent<Animator>();
+          //player = GetComponent<PlayerUi>();
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+          var moveInput = player.Direction;
+          anim.SetFloat("inputX", moveInput.x);
+          anim.SetFloat("inputY", moveInput.y);
+      }
+  }
+}
+
+
